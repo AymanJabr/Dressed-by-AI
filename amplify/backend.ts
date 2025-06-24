df16ca52b866a8809e1903c438bbe663ad79f28c
AymanJabr <aymanjaber2012@gmail.com>
2025-06-24 11:52:30 +0300

    chore: add data to the backend.ts

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,10 @@
 import { defineBackend } from '@aws-amplify/backend';
+import { data } from './data/resource';
 import { generateImage } from './functions/generate-image/resource';
 
 
 // define the backend and expose the function as a public API
 defineBackend({
-    generateImage
+    generateImage,
+    data,
 });
