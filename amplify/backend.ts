c0d82665953991e01d09f3af6a59dcc2bd4cdbca
AymanJabr <aymanjaber2012@gmail.com>
2025-06-25 10:07:41 +0300

    chore: import ts files as js

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,6 +1,6 @@
 import { defineBackend } from '@aws-amplify/backend';
-import { data } from './data/resource.ts';
-import { generateImage } from './functions/generate-image/resource.ts';
+import { data } from './data/resource.js';
+import { generateImage } from './functions/generate-image/resource.js';
 
 
 // define the backend and expose the function as a public API
